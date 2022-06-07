f563b365c465c4eddd1ee482e0ad4e83c4d86b9f
moamen2020 <m01144944981@gmail.com>
2022-06-07 09:54:28 +0200

    adding BrandFeatured section in HomePage

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -4,6 +4,7 @@ import Slider from '../../Components/Home/Slider'
 import NavBarLogin from '../../Components/Utility/NavBarLogin'
 import CardProductsContainer from '../../Components/Products/CardProductsContainer'
 import DiscountSection from '../../Components/Home/DiscountSection'
+import BrandFeatured from '../../Components/Brand/BrandFeatured'
 
 const HomePage = () => {
   return (
@@ -14,6 +15,7 @@ const HomePage = () => {
       <CardProductsContainer title='الأكثر مبيعاً' btntitle='المزيد' />
       <DiscountSection />
       <CardProductsContainer title='أحدث الأزياء' btntitle='المزيد' />
+      <BrandFeatured title="اشهر الماركات" btntitle="المزيد" pathText='brand' />
     </div>
   )
 }
