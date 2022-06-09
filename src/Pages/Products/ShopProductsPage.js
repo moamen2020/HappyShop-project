8c98b3cc3f23f89ceb124f3f914da829e45ea8e2
moamen2020 <m01144944981@gmail.com>
2022-06-09 13:40:29 +0200

    View Products Search Page Part2

--- src/Pages/Products/ShopProductsPage.js
+++ src/Pages/Products/ShopProductsPage.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import CategorysHeader from "../../Components/Category/CategoryHeader";
 import SearchCountResult from "../../Components/Utility/SearchCountResult";
+import SideFilter from "../../Components/Utility/SideFilter";
+import CardProductsContainer from "../../Components/Products/CardProductsContainer";
+import PaginationCompontent from "../../Components/Utility/Pagination";
 
 const ShopProductsPage = () => {
   return (
@@ -9,6 +12,16 @@ const ShopProductsPage = () => {
       <CategorysHeader />
       <Container>
         <SearchCountResult title="400 نتيجة بحث" />
+        <Row className="d-flex flex-row">
+          <Col sm="2" xs="2" md="1" className="d-flex">
+            <SideFilter />
+          </Col>
+
+          <Col sm="10" xs="10" md="11">
+            <CardProductsContainer />
+          </Col>
+        </Row>
+        <PaginationCompontent />
       </Container>
     </div>
   );
