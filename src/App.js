a1655b3efd6942e7e4bb99eb6d53a17ad6275291
moamen2020 <m01144944981@gmail.com>
2022-06-09 11:23:10 +0200

    Login and Register Pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBarLogin from "./Components/Utility/NavBarLogin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Utility/Footer";
 import LoginPage from "./Pages/Auth/LoginPage";
+import Rigester from "./Pages/Auth/RigesterPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<Rigester />} />
         </Routes>
       </BrowserRouter>
       <Footer />
