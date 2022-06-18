3c322ee79329f32b93db72085444ac9f81072242
moamen2020 <m01144944981@gmail.com>
2022-06-18 10:20:27 +0200

    Add Category (Admin) Part 1

--- src/Components/Admin/AdminAddCategory.js
+++ src/Components/Admin/AdminAddCategory.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import avatar from "../../images/avatar.png";
 const AdminAddCategory = () => {
+  const [img, setImg] = useState(avatar);
+  const [name, setName] = useState("");
+
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImg(URL.createObjectURL(event.target.files[0]));
+    }
+  };
+
+  const handelSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <div>
       <Row className="justify-content-start ">
         <div className="admin-content-text pb-4">اضافه تصنيف جديد</div>
         <Col sm="8">
           <div className="text-form pb-2">صوره التصنيف</div>
-          <img src={avatar} alt="" height="100px" width="120px" />
+          <div>
+            <label for="upload-photo">
+              <img
+                src={img}
+                alt="fzx"
+                height="100px"
+                width="120px"
+                style={{ cursor: "pointer" }}
+              />
+            </label>
+            <input
+              type="file"
+              name="photo"
+              onChange={onImageChange}
+              id="upload-photo"
+            />
+          </div>
           <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
             type="text"
             className="input-form d-block mt-3 px-3"
             placeholder="اسم التصنيف"
@@ -18,7 +48,9 @@ const AdminAddCategory = () => {
       </Row>
       <Row>
         <Col sm="8" className="d-flex justify-content-end ">
-          <button className="btn-save d-inline mt-2 ">حفظ التعديلات</button>
+          <button onClick={handelSubmit} className="btn-save d-inline mt-2 ">
+            حفظ التعديلات
+          </button>
         </Col>
       </Row>
     </div>
