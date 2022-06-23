24c8100a0653ae8251e6099d2cdfefa3cdfb57b6
moamen2020 <m01144944981@gmail.com>
2022-06-24 01:03:58 +0200

    Setup Brand Section Redux Axios

---
+++ src/Redux/action/brandAction.js
@@ -0,0 +1,57 @@
+import useGetData from "../../hooks/useDetData";
+import { useInsertDataWithImage } from "../../hooks/useInsertData";
+import { GET_ALL_BRAND, GET_ERROR, CREATE_BRAND } from "../type";
+
+// Get all Brand
+export const getAllBrand = (limit) => async (dispatch) => {
+  try {
+    const response = await useGetData(`/api/v1/brands?limit=${limit}`);
+    console.log(response);
+
+    dispatch({
+      type: GET_ALL_BRAND,
+      payload: response,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: "Error " + e,
+    });
+  }
+};
+
+// Get all Brand with pagination
+export const getAllBrandPage = (page) => async (dispatch) => {
+  try {
+    const response = await useGetData(`/api/v1/brands?limit=6&page=${page}`);
+
+    dispatch({
+      type: GET_ALL_BRAND,
+      payload: response,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: "Error " + e,
+    });
+  }
+};
+
+// Create new Brand
+export const createBrandPage = (formData) => async (dispatch) => {
+  try {
+    const response = await useInsertDataWithImage(`/api/v1/brands`, formData);
+    console.log(response);
+
+    dispatch({
+      type: CREATE_BRAND,
+      payload: response,
+      loading: true,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: "Error " + e,
+    });
+  }
+};
--- src/Redux/type.js
+++ src/Redux/type.js
@@ -1,3 +1,8 @@
+// Category
 export const GET_ALL_CATEGORY = "GET_ALL_CATEGORY";
 export const CREATE_CATEGORY = "CREATE_CATEGORY";
 export const GET_ERROR = "GET_ERROR";
+
+// Brand
+export const GET_ALL_BRAND = "GET_ALL_BRAND";
+export const CREATE_BRAND = "CREATE_BRAND";
