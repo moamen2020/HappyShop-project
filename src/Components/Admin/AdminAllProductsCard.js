c32c17d0242fcb88711b925f752101d9d468080b
moamen2020 <m01144944981@gmail.com>
2022-07-01 02:45:55 +0200

    Delete Products (API) Part 3

--- src/Components/Admin/AdminAllProductsCard.js
+++ src/Components/Admin/AdminAllProductsCard.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { Col, Row, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Row, Card, Modal, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import prod1 from "../../images/prod1.png";
+import { useDispatch } from "react-redux";
+import { deleteProduct } from "../../Redux/action/productAction";
+
 const AdminAllProducsCard = ({ item }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const dispatch = useDispatch();
+
+  const handelDelete = async () => {
+    await dispatch(deleteProduct(item._id));
+    setShow(false);
+    window.location.reload();
+  };
   return (
     <Col xs="12" sm="6" md="5" lg="4" className="d-flex">
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>
+            <div className="font">تأكيد الحذف</div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="font">هل أنت متأكد من عملية حذف هذا المنتج</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            تراجع
+          </Button>
+          <Button variant="primary" onClick={handelDelete}>
+            حذف
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <Card
         className="my-2"
         style={{
@@ -17,11 +50,13 @@ const AdminAllProducsCard = ({ item }) => {
       >
         <Row className="d-flex justify-content-center px-2">
           <Col className=" d-flex justify-content-between">
-            <div className="d-inline item-delete-edit">ازاله</div>
+            <div className="d-inline item-delete-edit" onClick={handleShow}>
+              ازاله
+            </div>
             <div className="d-inline item-delete-edit">تعديل</div>
           </Col>
         </Row>
-        <Link to="/products/:id" style={{ textDecoration: "none" }}>
+        <Link to={`/products/${item._id}`} style={{ textDecoration: "none" }}>
           <Card.Img
             style={{ height: "228px", width: "100%" }}
             src={item.imageCover}
