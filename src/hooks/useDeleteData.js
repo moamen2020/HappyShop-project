33a8e6f9a94bcc49ea9e2d5b649a0bf6fdb863cc
moamen2020 <m01144944981@gmail.com>
2022-07-15 05:28:17 +0200

    Make Request As Admin

--- src/hooks/useDeleteData.js
+++ src/hooks/useDeleteData.js
@@ -1,8 +1,10 @@
 import baseURL from "../Api/baseURL";
 
 const useDeleteData = async (url, parmas) => {
-  const res = await baseURL.delete(url, parmas);
+  const config = {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  };
+  const res = await baseURL.delete(url, parmas, config);
   return res.data;
 };
-
 export default useDeleteData;
