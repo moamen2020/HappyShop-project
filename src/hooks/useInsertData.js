74f601fb70ee83c61cd5d201ee5465d80d38790d
moamen2020 <m01144944981@gmail.com>
2022-07-15 05:26:19 +0200

    Make Request As Admin

--- src/hooks/useInsertData.js
+++ src/hooks/useInsertData.js
@@ -2,15 +2,23 @@ import baseURL from "../Api/baseURL";
 
 const useInsertDataWithImage = async (url, parmas) => {
   const config = {
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   };
   const res = await baseURL.post(url, parmas, config);
   return res;
 };
 
 const useInsertData = async (url, parmas) => {
-  const res = await baseURL.post(url, parmas);
-  return res.data;
+  const config = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  };
+  const res = await baseURL.post(url, parmas, config);
+  return res;
 };
 
 export { useInsertData, useInsertDataWithImage };
--- src/hooks/useUpdateData.js
+++ src/hooks/useUpdateData.js
@@ -2,15 +2,22 @@ import baseURL from "../Api/baseURL";
 
 const useUpdateDataWithImage = async (url, parmas) => {
   const config = {
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   };
   const res = await baseURL.put(url, parmas, config);
+  console.log(res.status);
   return res;
 };
 
 const useUpdateData = async (url, parmas) => {
-  const res = await baseURL.post(url, parmas);
-  return res.data;
+  const config = {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  };
+  const res = await baseURL.put(url, parmas, config);
+  return res;
 };
 
 export { useUpdateData, useUpdateDataWithImage };
