d8ec54800967857a4677cd5eb83fc5f2f0940397
moamen2020 <m01144944981@gmail.com>
2022-07-16 09:49:48 +0200

    User WishList Page

--- src/Components/User/UserFavoriteProduct.js
+++ src/Components/User/UserFavoriteProduct.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
-import ProductCard from "../Products/ProductCard";
-import PaginationCompontent from "../Utility/Pagination";
+import CardProductsContainer from "./../Products/CardProductsContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductWishList } from "../../Redux/action/wishListAction";
 
 const UserFavoriteProduct = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    const get = async () => {
+      setLoading(true);
+      await dispatch(getProductWishList());
+      setLoading(false);
+    };
+    get();
+  }, []);
+
+  const res = useSelector((state) => state.addToWishListReducer.allWishList);
+  useEffect(() => {
+    if (loading === false) {
+      if (res) setItems(res.data);
+    }
+  }, [loading]);
+
   return (
     <Row className="justify-content-between ">
       <div className="admin-content-text">المنتجات المفضله</div>
       <Row className="justify-content-between ">
-        <ProductCard title="1" />
-        <ProductCard title="2" />
-        <ProductCard title="3" />
-        <ProductCard title="4" />
-        <ProductCard title="4" />
-        <ProductCard title="4" />
-        <ProductCard title="4" />
-        <ProductCard title="4" />
-        <PaginationCompontent />
+        {items.length <= 0 ? (
+          <h6>لا يوجد منتدات مفضله حاليا</h6>
+        ) : (
+          <CardProductsContainer products={items} title="" btntitle="" />
+        )}
       </Row>
     </Row>
   );
