bc4c6fd3c46ab392dc284a8de8df5356fb0d1950
moamen2020 <m01144944981@gmail.com>
2022-07-16 04:41:24 +0200

    Delete Review Part 2

--- src/Components/Rate/RateItem.js
+++ src/Components/Rate/RateItem.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Row, Col, Modal, Button } from "react-bootstrap";
 import rate from "../../images/rate.png";
 import deleteicon from "../../images/delete.png";
 import editicon from "../../images/edit.png";
+import DeleteRateHook from "../../hook/review/delete-rate-hook";
 const RateItem = ({ review }) => {
+  const [isUser, showDelete, handelDelete, handleShow, handleClose] =
+    DeleteRateHook();
   return (
     <div>
+      <Modal show={showDelete} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>
+            {" "}
+            <div className="font">تاكيد الحذف</div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="font">هل انتا متاكد من حذف التقييم</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="font" variant="success" onClick={handleClose}>
+            تراجع
+          </Button>
+          <Button className="font" variant="dark" onClick={handelDelete}>
+            حذف
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <Row className="mt-3">
         <Col className="d-felx me-5">
           <div className="rate-name  d-inline ms-2">{review.user.name}</div>
@@ -16,23 +39,26 @@ const RateItem = ({ review }) => {
       <Row className="border-bottom mx-2">
         <Col className="d-flex justify-content-between me-4 pb-2">
           <div className="rate-description  d-inline ms-2">{review.review}</div>
-          <div className="d-inline ">
-            <img
-              src={deleteicon}
-              width="20px"
-              height="20px"
-              style={{ cursor: "pointer" }}
-              alt="delete"
-            />
+          {isUser === true ? (
+            <div className="d-inline d-flex justify-content-end">
+              <img
+                src={deleteicon}
+                onClick={handleShow}
+                width="20px"
+                height="20px"
+                style={{ cursor: "pointer" }}
+                alt="delete"
+              />
 
-            <img
-              src={editicon}
-              width="20px"
-              height="20px"
-              style={{ cursor: "pointer" }}
-              alt="delete"
-            />
-          </div>
+              <img
+                src={editicon}
+                width="20px"
+                height="20px"
+                style={{ cursor: "pointer" }}
+                alt="delete"
+              />
+            </div>
+          ) : null}
         </Col>
       </Row>
     </div>
---
+++ src/hook/review/delete-rate-hook.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import notify from "../useNotifaction";
+import { deleteReviewOnProduct } from "./../../Redux/action/reviewAction";
+
+const DeleteRateHook = (review) => {
+  const dispatch = useDispatch();
+
+  const [isUser, setIsUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const [showDelete, setShowDelete] = useState(false);
+  const handleClose = () => setShowDelete(false);
+  const handleShow = () => setShowDelete(true);
+
+  let user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    if (review.user._id === user._id) {
+      setIsUser(true);
+    }
+  }, []);
+
+  const handelDelete = async () => {
+    setLoading(true);
+    await dispatch(deleteReviewOnProduct(review._id));
+    setLoading(false);
+    handleClose();
+  };
+  const res = useSelector((state) => state.reviewReducer.deleteReview);
+
+  useEffect(() => {
+    if (loading === false) {
+      if (res === "") {
+        notify("تم حذف التقييم بنجاح", "success");
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 1000);
+      } else notify("هناك مشكله فى عملية المسح", "error");
+    }
+  }, [loading]);
+
+  return [isUser, showDelete, handelDelete, handleShow, handleClose];
+};
+
+export default DeleteRateHook;
