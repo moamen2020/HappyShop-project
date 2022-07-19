abf79c01b29584925832236b793f76a12283da67
moamen2020 <m01144944981@gmail.com>
2022-07-19 16:49:25 +0200

    Show All User Cart Items

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -3,14 +3,21 @@ import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import mobile from "../../images/mobile.png";
 import deleteicon from "../../images/delete.png";
-const CartItem = () => {
+const CartItem = ({ item }) => {
   return (
     <Col xs="12" className="cart-item-body my-2 d-flex px-2">
-      <img width="160px" height="197px" src={mobile} alt="" />
+      <img
+        width="160px"
+        height="197px"
+        src={item.product.imageCover || mobile}
+        alt=""
+      />
       <div className="w-100">
         <Row className="justify-content-between">
           <Col sm="12" className=" d-flex flex-row justify-content-between">
-            <div className="d-inline pt-2 cat-text">الالكترونيات</div>
+            <div className="d-inline pt-2 cat-text">
+              {item.product.category.name || ""}
+            </div>
             <div className="d-flex pt-2 " style={{ cursor: "pointer" }}>
               <img src={deleteicon} alt="" width="20px" height="24px" />
               <div className="cat-text d-inline me-2">ازاله</div>
@@ -20,31 +27,29 @@ const CartItem = () => {
         <Row className="justify-content-center mt-2">
           <Col sm="12" className=" d-flex flex-row justify-content-start">
             <div className="d-inline pt-2 cat-title">
-              آيفون XR بذاكرة سعة 128 جيجابايت ويدعم تقنية 4G LTE مع تطبيق فيس
+              {item.product.title || ""}
             </div>
-            <div className="d-inline pt-2 cat-rate me-2">4.5</div>
+            <div className="d-inline pt-2 cat-rate me-2">
+              {item.product.ratingsAverage || 0}
+            </div>
           </Col>
         </Row>
         <Row>
           <Col sm="12" className="mt-1">
             <div className="cat-text d-inline">الماركة :</div>
-            <div className="barnd-text d-inline mx-1">ابل </div>
+            <div className="barnd-text d-inline mx-1">
+              {item.product.brand.name || ""}
+            </div>
           </Col>
         </Row>
         <Row>
           <Col sm="12" className="mt-1 d-flex">
-            <div
-              className="color ms-2 border"
-              style={{ backgroundColor: "#ACACAC" }}
-            ></div>
-            <div
-              className="color ms-2 border"
-              style={{ backgroundColor: "#E52C2C" }}
-            ></div>
-            <div
-              className="color ms-2 border"
-              style={{ backgroundColor: "#000" }}
-            ></div>
+            {item.color === "" ? null : (
+              <div
+                className="color ms-2 border"
+                style={{ backgroundColor: `${item.color}` }}
+              ></div>
+            )}
           </Col>
         </Row>
 
@@ -53,12 +58,15 @@ const CartItem = () => {
             <div className="d-inline pt-2 d-flex">
               <div className="cat-text  d-inline">الكميه</div>
               <input
+                value={item.count}
                 className="mx-2 "
                 type="number"
                 style={{ width: "40px", height: "25px" }}
               />
             </div>
-            <div className="d-inline pt-2 barnd-text">٣٠٠٠ جنية</div>
+            <div className="d-inline pt-2 barnd-text">
+              {item.price || 0} جنية
+            </div>
           </Col>
         </Row>
       </div>
--- src/Pages/Cart/CartPage.js
+++ src/Pages/Cart/CartPage.js
@@ -2,8 +2,16 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CartItem from "../../Components/Cart/CartItem";
 import CartCheckout from "../../Components/Cart/CartCheckout";
+import GetAllUserCartHook from "../../hook/cart/get-all-user-cart-hook";
 
 const CartPage = () => {
+  const [
+    itemsNum,
+    cartItems,
+    totalCartPrice,
+    couponNameRes,
+    totalCartPriceAfterDiscount,
+  ] = GetAllUserCartHook();
   return (
     <Container style={{ minHeight: "670px" }}>
       <Row>
@@ -11,12 +19,20 @@ const CartPage = () => {
       </Row>
       <Row className="d-flex justify-content-center">
         <Col xs="12" md="9">
-          <CartItem />
-          <CartItem />
-          <CartItem />
+          {cartItems.length >= 1 ? (
+            cartItems.map((item, index) => {
+              return <CartItem key={index} item={item} />;
+            })
+          ) : (
+            <h6>لا يوجد منتجات فى العربة</h6>
+          )}
         </Col>
         <Col xs="6" md="3">
-          <CartCheckout />
+          <CartCheckout
+            couponNameRes={couponNameRes}
+            totalCartPriceAfterDiscount={totalCartPriceAfterDiscount}
+            totalCartPrice={totalCartPrice}
+          />
         </Col>
       </Row>
     </Container>
