844ee9ffedc95ba9b412f24b00dd179cf2349c6f
moamen2020 <m01144944981@gmail.com>
2022-07-22 20:19:45 +0200

    View Admin All Orders

--- src/Components/Admin/AdminAllOrder.js
+++ src/Components/Admin/AdminAllOrder.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { Row } from "react-bootstrap";
 import AdminAllOrderItem from "./AdminAllOrderItem";
+import UserGetAllOrderHook from "./../../hook/user/user-get-all-order-hook";
+import PaginationComponent from "../Utility/Pagination";
+
 const AdminAllOrder = () => {
+  const [userName, results, paginate, orderData, onPress] =
+    UserGetAllOrderHook();
+
   return (
     <div>
-      <Row className="justify-content-between ">
-        <div className="admin-content-text">اداره جميع الطلبات</div>
-        <AdminAllOrderItem />
-        <AdminAllOrderItem />
-        <AdminAllOrderItem />
-        <AdminAllOrderItem />
+      <div className="admin-content-text">ادارة جميع الطلبات</div>
+      <Row className="justify-content-start">
+        {orderData.length >= 1 ? (
+          orderData.map((orderItem, index) => {
+            return <AdminAllOrderItem key={index} orderItem={orderItem} />;
+          })
+        ) : (
+          <h6>لا يوجد طلبات حتى </h6>
+        )}
+
+        {paginate.numberOfPages >= 2 ? (
+          <PaginationComponent
+            onPress={onPress}
+            pageCount={paginate.numberOfPages ? paginate.numberOfPages : 0}
+          />
+        ) : null}
       </Row>
     </div>
   );
